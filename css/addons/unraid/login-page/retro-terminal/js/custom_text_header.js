51493786929f96f346ff8e73357f6a75b4119340
Marcelthemaster <151378337+Marcelthemaster@users.noreply.github.com>
2025-01-12 21:40:25 +0100

    Update custom_text_header.js

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -7,20 +7,10 @@
 
 let custom_text_header = `
 <div class="custom-text-header"> <pre>
-E)eeeeee    k)               t) E)eeeeee           ##                       ##             
-E)          k)             t)tTTE)                                                         
-E)eeeeec)CCCk)  Ke)EEEEEr)RRRt) E)eeeeen)NNNN g)GGGin)NNNNe)EEEEe)EEEEEr)RRRin)NNNN g)GGG  
-E)    c)    k)KK e)EEEEr)   Rt) E)     n)   Ng)   Gin)   Ne)EEEEe)EEEEr)   Rin)   Ng)   GG 
-E)    c)    k) KKe)    r)    t) E)     n)   Ng)   Gin)   Ne)    e)    r)    in)   Ng)   GG 
-E)eeeeec)CCCk)  KKe)EEEr)    t)TE)eeeeen)   NNg)GGGin)   NNe)EEEEe)EEEr)    in)   NNg)GGGG 
-                                                  GG                                    GG 
-                                             g)GGGG                                g)GGGG  
- 
   __|       |             |    __|              _)                       _)              
   _|    _|  | /   -_)   _| _|  _|     \    _` |  |    \    -_)   -_)   _| |    \    _` | 
  ___| \__| _\_\ \___| _| \__| ___| _| _| \__, | _| _| _| \___| \___| _|  _| _| _| \__, | 
-                                         ____/                                    ____/  
-                                            
+                                         ____/                                    ____/                                            
 </pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
